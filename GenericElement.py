a46524490828c366bf7d9744d9643a48c59c4b5a
ZigRazor <zigrazor@gmail.com>
2021-10-06 15:58:23 +0000

    First Definition Of Base Class
    Signed-off-by: ZigRazor <zigrazor@gmail.com>

---
+++ GenericElement.py
@@ -0,0 +1,43 @@
+class GenericElement:
+    """GenericElement Class"""
+    def __init__(self):
+        """Constructor of Generic Element Class"""
+        self.element_name = ""
+        self.format = ""
+    
+    def set_element_name(self, element_name):
+        """Setter for element name"""
+        self.element_name = element_name
+
+    def set_format(self, format):
+        """Setter for format"""
+        self.format = format
+
+    def get_element_name(self):
+        """Getter for element name"""
+        return self.element_name
+
+    def get_format(self):
+        """Getter for format"""
+        return self.format
+
+    def __str__(self):
+        """String representation of the class"""
+        return self.element_name + " " + self.format
+
+    def __repr__(self):
+        """Representation of the class"""
+        return self.element_name + " " + self.format
+
+    def __eq__(self, other):
+        """Equality operator"""
+        return self.element_name == other.element_name and self.format == other.format
+    
+    def __ne__(self, other):
+        """Inequality operator"""
+        return not self.__eq__(other)
+
+    def __hash__(self):
+        """Hash function"""
+        return hash(self.element_name) ^ hash(self.format)
+
---
+++ Severity.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+import GenericElement
+
+class Severity(Enum,GenericElement.GenericElement):
+    """
+    Severity of the Log.
+    """
+    TRACE = 1
+    DEBUG = 2
+    INFO = 3
+    ERROR = 4
+    CRITICAL = 5
