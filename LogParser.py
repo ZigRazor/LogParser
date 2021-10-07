c8e435fb861c1b682aa80b1a4be3520ddee39e4a
ZigRazor <zigrazor@gmail.com>
2021-10-07 13:34:18 +0000

    Date Formatting
    Introduced for the first time the Date Class in the Elements
    Formatted date now are read.
    
    Signed-off-by: ZigRazor <zigrazor@gmail.com>

--- LogParser.py
+++ LogParser.py
@@ -1,3 +1,6 @@
+import re
+import GenericElement
+
 class LogParser:
     """LogParser Class"""
     def __init__(self):
@@ -42,18 +45,65 @@ class LogParser:
         """Get key list string"""
         return '\n'.join(self.keys)
 
-    def add_key(self, key):
+    def add_key(self, key: str):
         """Add key to keys"""
+        if(key not in self.elements):
+            raise Exception("Key not present in Elements")
+        if(key in self.keys):
+            raise Exception("Key already present in Keys")
         self.keys.append(key)
 
-    def remove_key(self, key):
+    def remove_key(self, key: str):
         """Remove key from keys"""
         self.keys.remove(key)
+
+    def clear_keys(self):
+        """Clear keys"""
+        self.keys.clear()
     
+    def clear_elements(self):
+        """Clear elements"""
+        self.elements.clear()
+        self.keys.clear()   
+
+    def get_element_count(self):
+        """Get element count"""
+        return len(self.elements)
+
+    def get_element_list(self):
+        """Get element list"""
+        return self.elements
+
+    def get_element_list_string(self):
+        """Get element list string"""
+        return '\n'.join(self.elements)
+    
+    def add_element(self, key: str, value: GenericElement.GenericElement):
+        """Add element to elements"""
+        self.elements[key] = value
+
+    def remove_element(self, key: str):
+        """Remove element from elements"""
+        self.elements.pop(key)
+        if key in self.keys:
+            self.keys.remove(key)
+            
     def parse_log(self):
         """Parse log files"""
+        result = {}
+        for key in self.keys:
+            result[key] = {}
         for file in self.file_list:
             with open(file, 'r') as f:
                 for line in f:
-                    #TODO: parse line
-                    pass
+                    print (line)
+                    for element in self.elements.items():
+                        print (element)
+                        regex = re.compile(element[1].get_format())
+                        print (regex)
+                        match = regex.search(line)
+                        element[1].set_date_from_string(match.group())
+                        print (match)
+                        print (element)
+                        #pass
+        return True
---
+++ MainTest.py
@@ -0,0 +1,25 @@
+from DateFormat import DateFormat
+import LogParser
+import Date
+import re
+
+
+def main():
+    log_parser = LogParser.LogParser()
+    log_parser.add_file("./test1.txt");
+
+    date = Date.Date()
+    date.set_element_name("date")
+    date.set_format(r'\d{4}-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])')
+    print(date.get_date_format())
+    date.set_date_format(DateFormat.YEAR_MONTH_DAY)
+    print(date.get_date_format())
+    log_parser.add_element("Date",date)
+    log_parser.add_key("Date")
+
+    a = log_parser.parse_log()
+
+    print(a)
+
+if __name__ == "__main__":
+    main()
